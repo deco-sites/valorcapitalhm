ef026aabc725061d05561469f68851418b565239
Kesler <kesler.m.pereira@gmail.com>
2023-07-29 14:47:03 -0300

    feat: under header finished

--- sections/UnderHeader.tsx
+++ sections/UnderHeader.tsx
@@ -1,28 +1,32 @@
-export interface props{
-    title: string;
-    text: string;
+export interface props {
+  title: string;
+  text: string;
 }
 
 export default function UnderHeader(
-    {
-        title,
-        text
-    }: props,
+  {
+    title,
+    text,
+  }: props,
 ) {
-    return (
-        <section className={"bg-[#F3F3F3]"}>
-            <div className={"flex flex-col justify-center max-w-[90%] lg:max-w-[80rem] mx-auto lg:px-[5rem] pb-[100px]"}>
-                <div
-                    dangerouslySetInnerHTML={{ __html: title }}
-                    className={"font-[700] lg:text-[72px] text-[48px] mb-[30px] text-black lg:max-w-[800px]"}
-                ></div>
-                <div>
-                    <p
-                        dangerouslySetInnerHTML={{ __html: text }}
-                        className={"text-lg text-left lg:text-[1.75rem] lg:max-w-[700px] lg:leading-normal"}
-                    ></p>
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section className={"bg-[#F3F3F3]"}>
+      <div
+        className={"flex flex-col justify-center max-w-[90%] lg:max-w-[80rem] mx-auto lg:px-[5rem] pb-[100px]"}
+      >
+        <div
+          dangerouslySetInnerHTML={{ __html: title }}
+          className={"font-[700] lg:text-[72px] text-[48px] mb-[30px] text-black lg:max-w-[800px]"}
+        >
+        </div>
+        <div>
+          <p
+            dangerouslySetInnerHTML={{ __html: text }}
+            className={"text-lg text-left lg:text-[1.75rem] lg:max-w-[700px] lg:leading-normal"}
+          >
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
